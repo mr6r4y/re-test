62157797a48f1af2f5f5be0d0959daf36b86168d
mr6r4y <mr6r4y@gmail.com>
2019-03-08 01:43:25 +0200

    Add

--- arm_test/write.s
+++ arm_test/write.s
@@ -1,14 +1,14 @@
-.data
-string: .asciz "Azeria Labs\n"
+.section .data
+    string: .asciz "Azeria Labs\n"
 
-.text
+.section .text
 .global _start
 
 _start:
     mov r0, #1                  @ STDOUT
     ldr r1, addr_of_string      @ address of string
     mov r2, #13                 @ size of string
-    mov r7, #7                  @ 'write' syscall
+    mov r7, #4                  @ 'write' syscall; found in /usr/include/x86_64-linux-gnu/asm/unistd_32.h
     swi #0                      @ invoke syscall
 
 _exit:
