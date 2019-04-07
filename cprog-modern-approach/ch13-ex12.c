2ce780d38c8ead055fe84efd6b0fe05e10193401
mr6r4y <mr6r4y@gmail.com>
2019-04-07 09:48:53 +0300

    Add

--- cprog-modern-approach/ch13-ex12.c
+++ cprog-modern-approach/ch13-ex12.c
@@ -1,10 +1,79 @@
+#include <ctype.h>
+#include <string.h>
 #include <stdio.h>
 
 
+void get_extension_v1(const char *filename, char *ext)
+{
+	int i;
+	
+	i =  strlen(filename);
+	while (i >= 0) {
+		if (filename[i] == '.') {
+			strncpy(ext, filename + i + 1, 10);
+			return;
+		}
+		i--;
+	}
 
+	*ext = '\0';
+}
+
+
+char * get_extension_v2(const char *filename)
+{
+	int len, i;
+	char *ext;
+	
+	i = len =  strlen(filename);
+	ext = filename + len;
+
+	while (i >= 0) {
+		if (*ext == '.') {
+			ext++;
+			return ext;
+		}
+		ext--; 
+		i--;
+	}
+
+	return filename + len;
+}
+
+
+int read_line(char str[], int n)
+{
+ 	int ch, i = 0;
+
+ 	while (isspace(ch = getchar()));
+ 	while (ch != '\n' && ch != EOF) {
+ 		if (i < n) str[i++] = ch;
+ 		ch = getchar();
+  	}
+	str[i] = '\0';
+	return i;
+}
 
 
 int main()
-{
+{	
+	char e[100] = {};
 
+	// get_extension("memo.txt", e);
+	// printf("%s\n", e);
+
+	// get_extension("memo", e);
+	// printf("%s\n", e);
+
+	#define N 100
+	char buf[N] = {};
+
+	while(1) {
+		printf("Enter filename: ");
+		read_line(buf, N - 1);
+		if(*buf == '\0') 
+			break;
+		// get_extension_v1(buf, e);
+		printf("%s\n", get_extension_v2(buf));
+	}
 }
---
+++ cprog-modern-approach/ch13-ex15.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int f(char *s, char *t)
+{
+	char *p1, *p2;
+	for (p1 = s; *p1; p1++) {
+		for (p2 = t; *p2; p2++)
+			if (*p1 == *p2) break;
+		if (*p2 == '\0') break;
+	}
+
+	return p1 - s;
+}
+
+
+int main(void)
+{
+	printf("f(abcd, babc)=%d\n", f("abcd", "babc"));
+	printf("f(abcd, bcd)=%d\n", f("abcd", "bcd)"));
+}
