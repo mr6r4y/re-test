3b29c221d0399c1e54a42bd8c1d7aec4bf22f39f
mr6r4y <mr6r4y@gmail.com>
2019-04-20 10:01:13 +0300

    Finish ch13 excercises

--- cprog-modern-approach/ch13-pr18.c
+++ cprog-modern-approach/ch13-pr18.c
@@ -1,6 +1,8 @@
 #include <ctype.h>
 #include <string.h>
+
 #include <stdio.h>
+#include <stdlib.h>
 
 
 int read_line(char str[], int n)
@@ -17,9 +19,74 @@ int read_line(char str[], int n)
 }
 
 
-void date_convert(const char *date_to_convert, char *converted_date)
+#define ERROR_DATE_CONVERT -1
+#define SUCCESS_DATE_CONVERT 1
+
+
+int date_convert(const char *date_to_convert, char *converted_date)
 {
-	strcpy(converted_date, date_to_convert);
+	int dd = 0, mm = 0, yyyy = 0;
+	sscanf(date_to_convert, "%02d/%02d/%d", &mm, &dd, &yyyy);
+
+	if (!dd || !mm || !yyyy)
+		return ERROR_DATE_CONVERT;
+
+	if (mm > 12 || mm < 1)
+		return ERROR_DATE_CONVERT;
+
+	char *month;
+	switch (mm) {
+		case 1:
+		month = "Jan";
+		break;
+
+		case 2:
+		month = "Feb";
+		break;
+
+		case 3:
+		month = "Mar";
+		break;
+
+		case 4:
+		month = "Apr";
+		break;
+
+		case 5:
+		month = "May";
+		break;
+
+		case 6:
+		month = "Jun";
+		break;
+
+		case 7:
+		month = "Jul";
+		break;
+
+		case 8:
+		month = "Aug";
+		break;
+
+		case 9:
+		month = "Sep";
+		break;
+
+		case 10:
+		month = "Oct";
+		break;
+
+		case 11:
+		month = "Nov";
+		break;
+
+		case 12:
+		month = "Dec";
+		break;
+	}
+
+	sprintf(converted_date, "%s %d, %d", month, dd, yyyy);
+	return SUCCESS_DATE_CONVERT;
 }
 
 
@@ -28,14 +95,22 @@ int main()
 	#define N 100
 	char buf[N];
 	char date[N];
+	int convert_res;
 
 	while(1) {
+		memset(buf, 0, N);
+		memset(date, 0, N);
 		printf("Enter a date (mm/dd/yyyy): ");
 		read_line(buf, N - 1);
-		if(*buf == '\0') 
+		if(*buf == '\0')
 			break;
-		else
-			date_convert(buf, date);
-		printf("You entered the date %s\n\n", date);
+		else {
+			convert_res = date_convert(buf, date);
+
+			if (convert_res == SUCCESS_DATE_CONVERT)
+				printf("You entered the date %s\n\n", date);
+			else
+				printf("You entered an invalid date!\n");
+		}
 	}
 }
